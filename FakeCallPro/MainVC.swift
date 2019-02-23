9a64a7a8b603288d365380bac6feef8adaa8ff38
Ankit Saxena <ankitatdelhii@ankits-macbook-air.local>
2019-02-24 03:13:54 +0530

    Main Settings TableView Setup Done!

--- FakeCallPro/MainVC.swift
+++ FakeCallPro/MainVC.swift
@@ -11,12 +11,14 @@ import UIKit
 class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var settingsTableView: UITableView!
-    var itemArray = [Int]()
+    var settingsArray = ["Time", "Caller", "Ring & Vibration", "Voice", "Wallpaper"]
+    var tempdefaultSettings = ["3 Seconds Later", "Ashish", "Opening", "Voice2", ""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         settingsTableView.delegate = self
         settingsTableView.dataSource = self
+        settingsTableView.separatorStyle = .none
     }
     
     @IBAction func startButtonPressed(_ sender: UIButton) {
@@ -27,12 +29,14 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = settingsTableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath) as! SettingsCell
-        cell.fLabel.text = "Working"
+        cell.fLabel.text = settingsArray[indexPath.row]
+        cell.sLabel.text = tempdefaultSettings[indexPath.row]
+        print(cell.fLabel.frame.height)
         return cell
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return settingsArray.count
     }
 }
