72bc974c6d314ef177ed74441f894b285b24e3b1
Ankit Saxena <ankitatdelhii@ankits-macbook-air.local>
2019-03-03 03:11:07 +0530

    Start Call Added! Autolayout Not Fixed!

---
+++ FakeCallPro/CallStarted/CallStartedVC.swift
@@ -0,0 +1,21 @@
+//
+//  CallStartedVC.swift
+//  FakeCallPro
+//
+//  Created by Ankit Saxena on 03/03/19.
+//  Copyright © 2019 Ankit Saxena. All rights reserved.
+//
+
+import UIKit
+
+class CallStartedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
